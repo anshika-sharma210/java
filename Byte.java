ad12ec2748e2a58ae07076dc2adbbe7532e2f56b
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2021-05-21 04:45:46 -0700

    Add files via upload

---
+++ Byte.java
@@ -0,0 +1,11 @@
+package Byte;
+
+public class ByteExample1 {
+public static void main(String[]args){
+byte num1 = 121;
+byte num2 = -128;
+
+System.out.println("num1:"+num1);
+System.out.println("num2:"+num2);
+}
+}
