36a346d29c25122746e0dd6c3048cbcf324eff27
anshika-sharma210 <72012561+anshika-sharma210@users.noreply.github.com>
2021-06-19 06:27:46 -0700

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,10 @@
+class SimpleObject {
+   SimpleObject()
+   {
+         System.out.println("Hello I am Constructor");  
+   }
+   public static void main(String []as)
+   {
+       SimpleObject obj=new SimpleObject();
+}
+}
