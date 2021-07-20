497c503a7f0b3e12470f88e8fbfabb15253e74f2
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-07-20 01:44:10 -0700

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,42 @@
+//Array is a container object that holds a fixed number of values of a single type.
+// Array is a group of like-typed of variable  that referred to by common name.
+package programs;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// declare An array of integers 
+		int[]anArray;
+		
+		// allocates memory for 10 integers 
+		anArray = new int[10];
+		
+		// initialize first element 
+		anArray[0] = 100;
+		// initialize second element 
+		anArray[1]= 200;
+		anArray[2]= 300;
+		anArray[3]= 400;
+		anArray[4]= 500;
+		anArray[5]= 600;
+		anArray[6]= 700;
+		anArray[7]= 800;
+		anArray[8]= 900;
+		anArray[9]= 1000;
+		System.out.println("Element of index 0: " +anArray[0]);
+		System.out.println("Element of index 1: " +anArray[1]);
+		System.out.println("Element of index 2: " +anArray[2]);
+		System.out.println("Element of index 3: " +anArray[3]);
+		System.out.println("Element of index 4: " +anArray[4]);
+		System.out.println("Element of index 5: " +anArray[5]);
+		System.out.println("Element of index 6: " +anArray[6]);
+		System.out.println("Element of index 7: " +anArray[7]);
+		System.out.println("Element of index 8: " +anArray[8]);
+		System.out.println("Element of index 9: " +anArray[9]);
+		
+		
+		
+		
+	}
+
+}
