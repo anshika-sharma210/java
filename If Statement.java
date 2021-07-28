5ed517d8141f75bddd907f9befcbdb6a4f31e0b4
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-07-27 21:00:56 -0700

    Add files via upload

---
+++ If Statement.java
@@ -0,0 +1,16 @@
+// the java if statement is the most simple decision making statement. it is used to decide  whether a certain statement  or block of statements will be executed or not i.e if the certain condition then a block of statements will be executed or not i.e if a certain condition is true a block of statement is executed otherwise not
+// if statement is the most basic of all the control flow statements . if statement tells our program to execute a certain section of code only if a particular test evaluates to true
+package programs;
+
+public class Ifprogram {
+ 
+	public static void main(String[] args) {
+		int num = 59;
+		if(num < 100) {
+			// if the above condition is true
+			System.out.println("number is less than 100");
+		}
+		}
+	}
+
+
