9f7eff1a882ad065afd3b1e7586d953a7e32b6b8
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-12 06:24:22 -0700

    Add files via upload

---
+++ child.java
@@ -0,0 +1,13 @@
+class Base
+{
+}
+ class child extends Base
+{
+public static void main(String args[])
+{
+child c1 = new child();
+System.out.println("c1 instanceof child");
+System.out.println("c1 instanceof Base");
+
+}
+}
