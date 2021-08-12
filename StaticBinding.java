2bd2e471a9a7b9528ca190c1a477446d5b80e387
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-12 07:07:49 -0700

    Add files via upload

---
+++ StaticBinding.java
@@ -0,0 +1,23 @@
+
+class Base{
+	int x=10;
+	}
+class child extends Base
+	{ 
+		int x =20;
+		void show()
+		{
+			
+		
+		System.out.println(x);
+	}
+	public static void main(String[] args) {
+		child c = new child();
+		c.show();
+	
+	}
+}
+
+
+
+
