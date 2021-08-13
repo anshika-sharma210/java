a427e51689b6b04c1f0a8beef76856e4d330b21a
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-13 09:45:22 -0700

    Add files via upload

---
+++ Base.java
@@ -0,0 +1,23 @@
+class Base
+{
+int x = 10;
+void show()
+{
+System.out.println(y);
+System.out.println("show of Base");
+}
+}
+Base()
+{
+System.out.println(x);
+show();
+}
+public static void main(String args[])
+{
+System.out.println(new . Base().x);
+Base b = new Base();
+System.out.println(b.n);
+new Base()show();
+b.show();
+}
+}
