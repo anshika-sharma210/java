c06caab0f9cb0198934e40a91131d929570b7c20
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-29 01:36:18 -0700

    Add files via upload

---
+++ sum2.java
@@ -0,0 +1,15 @@
+import java.lang.Integer;
+class sum2
+{
+public static void main(String args[])
+{
+int a = 40, b = 20;
+System.out.print("The sum of "+a+" and "+b+" is =");
+while(a>0)
+{
+b++;
+a--;
+}
+System.out.print(b);
+}
+}
