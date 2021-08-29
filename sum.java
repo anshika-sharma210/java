4e90963c4351bdcccdb300a584523a5a10bb83c4
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-29 01:35:04 -0700

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class sum
+{
+public static void main(String args[])
+{
+int sum, a = 15, b = 20 ;
+Scanner obj = new Scanner(System.in);
+a = obj.nextInt();
+b= obj.nextInt();
+sum = a+b;
+System.out.println("the sum of "+a+"and"+b+" is = "+sum) ;
+}
+}
