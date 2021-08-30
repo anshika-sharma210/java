f6efbde88b8730d5d2f7f740ce1e060ae554721f
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-30 06:52:00 -0700

    Add files via upload

---
+++ SwapTwoNumbers .java
@@ -0,0 +1,22 @@
+public class SwapTwoNumbers
+{
+public static voidmain(String[] args)
+{
+int a =  58, b = 45;
+// print the value of numbers before swapping
+System.out.println("--Before swapping--");
+System.out.println("First number = " + a);
+System.out.println("Second number = "+b);
+// Assign value of a temporary (third)variable
+int temp =a;
+//value of b will be stored in a
+a = b;
+// value of temp (temporary variable )initially contains a' value
+a's value assigned to b
+b = temp;
+// print the value of numbers after swapping
+System.out.println("\n\n--After swapping--");
+System.out.println("First number = " + a);
+System.out.println("Second number = "+b);
+}
+}
