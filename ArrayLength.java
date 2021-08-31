7d03e8f6fb26857bc7ac446e7e9b9204b7c89f9f
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-08-31 07:31:49 -0700

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,9 @@
+public class ArrayLength {
+public static void main(String[] args){
+//declare and initialize an array
+int arr[] = {10, 20, 30, 40, 50};
+//display the length
+System.out.println("The length of the given array=");
+System.out.println(arr.length);
+}
+}
