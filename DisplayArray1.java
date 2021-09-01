ffbe2ee180465054906d5fef693d820fc25de7b3
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-09-01 11:13:35 -0700

    Add files via upload

---
+++ DisplayArray1.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class DisplayArray1 {
+public static void main(String[] args) {
+// create Scanner class object to read input 
+Scanner scan= new Scanner(System.in);
+//declare an array variable of inttype
+int arr[] = null;
+// ask the length
+System.out.println("Enter array length: ");
+// assign size to the array
+arr = new int[length];
+// assign size to the arr
+arr = new int[length];
+// take input values
+System.out.println("Enter array elements: ");
+for(int i=0; i<array.length; i++)
+{
+arr[i]=scan.next Int();
+}
+// display the array using for loop  
+System.out.println("the Entered array : ");
+for(int  i = 0; i<arr.length; i++)
+{System.out.println(arr[i] +" ")
+}
+}
+}
