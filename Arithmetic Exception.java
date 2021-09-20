fee824823639ac911e7c47a86cfa99a6ff25b323
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-09-20 10:51:45 -0700

    Add files via upload

---
+++ Arithmetic Exception.java
@@ -0,0 +1,16 @@
+class ArithmeticException 
+{
+int i = 20;
+int j 30;
+public static void main(String ar[])
+int i = integer.parseInt(arr [0])
+int j = integer.parseInt(arr[1])
+int z = i+j;
+System.out.println(z);
+int z1=i/j;
+System.out.println(z1);
+int z2=i*j;
+System.out.println(z2);
+System.out.println(Rest of code);
+}
+}
