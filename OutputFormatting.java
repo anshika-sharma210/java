a08e1e359b6e91e7a09c09194ed776fad1e7a1c9
Anshika Sharma <72012561+anshika-sharma210@users.noreply.github.com>
2021-09-29 10:34:23 -0700

    Add files via upload

---
+++ OutputFormatting.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+public class OutputFormatting {
+
+public static void main(string[] args) {
+Scanner sc.new Scanner(System.in);
+System.out.println("==========");
+for(int i = 0; i<3; i++)
+{
+String s1=sc.next();
+int c= sc.nextInt();
+System.out.println("%-15s%x%o3\n",s1,x);
+}
+System.out.println("=========");
+}
+}
